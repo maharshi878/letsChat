c87b66489f469266889dec0b025fc7eb1a0c84bb
maharshi878 <88446251+maharshi878@users.noreply.github.com>
2021-08-04 21:02:44 +0530

    Created JS for login page

---
+++ SignInScript.js
@@ -0,0 +1,5 @@
+function regToStorage(){
+var userName = document.getElementById("userName_value");
+localStorage.setItem('UserName', userName);
+window.location = "maharshi878.github.io/letsChat/chatRoom.html"
+}
