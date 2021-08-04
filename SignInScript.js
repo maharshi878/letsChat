ee83b7e30bed2fa0b3174aae0710878eb9b97f42
maharshi878 <88446251+maharshi878@users.noreply.github.com>
2021-08-04 21:05:51 +0530

    Update SignInScript.js

--- SignInScript.js
+++ SignInScript.js
@@ -1,4 +1,4 @@
-function regToStorage(){
+function regUser(){
 var userName = document.getElementById("userName_value");
 localStorage.setItem('UserName', userName);
 window.location = "maharshi878.github.io/letsChat/chatRoom.html"
