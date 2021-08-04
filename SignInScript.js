50ab3453bc567f26ecb1987c918cfe903a21160a
maharshi878 <88446251+maharshi878@users.noreply.github.com>
2021-08-04 21:08:39 +0530

    Update SignInScript.js

--- SignInScript.js
+++ SignInScript.js
@@ -1,5 +1,5 @@
-function regUser(){
-var userName = document.getElementById("userName_value");
-localStorage.setItem('UserName', userName);
-window.location = "maharshi878.github.io/letsChat/chatRoom.html"
+function addUser(){
+SessionIdTemp = document.getElementById("userName")
+localStorage.setItem("SessionId", SessionIdTemp);
+window.location = "chatRoom.html";
 }
